17e7f11f485b5272456b4f61aed7c790048e1f96
Moyai <unmoyai@gmail.com>
2025-01-07 12:35:35 +0400

    add multi platfrom build

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,66 @@
-use clap::{Arg, Command};
-use winit::{
+use tao::{
     dpi::{LogicalPosition, LogicalSize},
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    platform::x11::WindowBuilderExtX11,
-    window::WindowBuilder,
+    platform::macos::WindowBuilderExtMacOS,
+    window::{Window, WindowBuilder},
 };
-use wry::webview::WebViewBuilder;
+use wry::{WebView, WebViewBuilder};
 
+#[cfg(any(
+    target_os = "windows",
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "android"
+))]
+fn get_webview(builder: WebViewBuilder, window: &Window) -> wry::Result<WebView> {
+    builder.build(window)
+}
+
+#[cfg(not(any(
+    target_os = "windows",
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "android"
+)))]
+fn get_webview(builder: WebViewBuilder, window: &Window) -> wry::Result<WebView> {
+    use tao::platform::unix::WindowExtUnix;
+    use wry::WebViewBuilderExtUnix;
+    let vbox = window.default_vbox().unwrap();
+    builder.build_gtk(vbox)
+}
+
+#[cfg(target_os = "macos")]
+fn get_window_builder() -> WindowBuilder {
+    use tao::platform::macos::WindowBuilderExtMacOS;
+    WindowBuilder::new().with_titlebar_hidden(true)
+}
 fn main() -> wry::Result<()> {
-    let matches = Command::new("Pano")
-        .arg(
-            Arg::new("width")
-                .short('w')
-                .long("width")
-                .default_value("800")
-                .value_parser(clap::value_parser!(u32)),
-        )
-        .arg(
-            Arg::new("height")
-                .short('h')
-                .long("height")
-                .default_value("600")
-                .value_parser(clap::value_parser!(u32)),
-        )
-        .arg(
-            Arg::new("x")
-                .short('x')
-                .long("x")
-                .default_value("100")
-                .value_parser(clap::value_parser!(i32)),
-        )
-        .arg(
-            Arg::new("y")
-                .short('y')
-                .long("y")
-                .default_value("100")
-                .value_parser(clap::value_parser!(i32)),
-        )
-        .arg(
-            Arg::new("url")
-                .short('u')
-                .long("url")
-                .default_value("data:text/html,"),
-        )
-        .get_matches();
-
-    let width = *matches.get_one::<u32>("width").unwrap();
-    let height = *matches.get_one::<u32>("height").unwrap();
-    let x = *matches.get_one::<i32>("x").unwrap();
-    let y = *matches.get_one::<i32>("y").unwrap();
-    let url = matches.get_one::<String>("url").unwrap();
-
-    // Create the Winit event loop
     let event_loop = EventLoop::new();
-
-    // Create a window with transparency and X11-specific settings
-    let window = WindowBuilder::new()
-        .with_title("Overlay App")
-        .with_inner_size(LogicalSize::new(width, height))
-        .with_position(LogicalPosition::new(x, y))
+    let window = get_window_builder()
+        .with_always_on_top(true)
         .with_transparent(true)
+        .with_position(LogicalPosition::new(0, 0))
+        .with_inner_size(LogicalSize::new(200, 200))
+        .with_visible(true)
         .build(&event_loop)
         .unwrap();
 
-    // Create a Wry WebView with a transparent background
-    let _webview = WebViewBuilder::new(window)
-        .unwrap()
-        .with_transparent(true)
-        .with_url(url)
-        .unwrap()
-        .build()
-        .unwrap();
+    let builder = WebViewBuilder::new()
+        .with_url("https://example.com/")
+        .with_transparent(true);
 
-    // Run the event loop
+    let _webview = get_webview(builder, &window)?;
+
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                *control_flow = ControlFlow::Exit;
-            }
-            _ => {}
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } = event
+        {
+            *control_flow = ControlFlow::Exit;
         }
     });
 }
