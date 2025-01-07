f0d73761b098aa0873c20144246b2c21a1cbd225
Moyai <unmoyai@gmail.com>
2025-01-07 16:02:10 +0400

    add run args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
+use std::path::PathBuf;
+
 use tao::{
     dpi::{LogicalPosition, LogicalSize},
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    platform::macos::WindowBuilderExtMacOS,
     window::{Window, WindowBuilder},
 };
 use wry::{WebView, WebViewBuilder};
+
+use clap::{ArgAction, Parser};
+
+#[derive(Debug, Parser)]
+#[command(name = "pano", version, about, disable_help_flag = true)]
+struct Options {
+    #[arg(short, long, default_value = "", help = "URL to open")]
+    url: String,
+
+    #[arg(short, long, default_value_t = 800, help = "Width of the window")]
+    width: u32,
+
+    #[arg(short, long, default_value_t = 600, help = "Height of the window")]
+    height: u32,
+
+    #[arg(short, default_value_t = 0, help = "X position of the window")]
+    x: i32,
+
+    #[arg(short, default_value_t = 0, help = "Y position of the window")]
+    y: i32,
+
+    #[arg(long, default_value_t = false, help = "Enable GPU acceleration")]
+    gpu: bool,
+
+    #[arg(long, default_value = "/tmp/pano", help = "Path to the socket file")]
+    socket: PathBuf,
+
+    #[arg(long, action = ArgAction::Help, help = "Print help information")]
+    help: Option<u8>,
+}
 
 #[cfg(any(
     target_os = "windows",
@@ -35,22 +66,25 @@ fn get_window_builder() -> WindowBuilder {
     use tao::platform::macos::WindowBuilderExtMacOS;
     WindowBuilder::new().with_titlebar_hidden(true)
 }
-fn main() -> wry::Result<()> {
+
+fn main() {
+    let options = Options::parse();
+
     let event_loop = EventLoop::new();
     let window = get_window_builder()
         .with_always_on_top(true)
         .with_transparent(true)
-        .with_position(LogicalPosition::new(0, 0))
-        .with_inner_size(LogicalSize::new(200, 200))
+        .with_position(LogicalPosition::new(options.x, options.y))
+        .with_inner_size(LogicalSize::new(options.width, options.height))
         .with_visible(true)
         .build(&event_loop)
         .unwrap();
 
     let builder = WebViewBuilder::new()
-        .with_url("https://example.com/")
+        .with_url(options.url)
         .with_transparent(true);
 
-    let _webview = get_webview(builder, &window)?;
+    let _webview = get_webview(builder, &window).unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
