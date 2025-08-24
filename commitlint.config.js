274f066fbffad86850b9573adb95a677c743de59
dockninja <wm@dock.ninja>
2025-08-24 17:05:59 +0100

    feat(ci): update CI workflow to include commit linting and semantic release configuration

---
+++ commitlint.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // only these types allowed
+    'type-enum': [
+      2, 'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'test',
+        'revert',
+        'chore',
+        'docs',
+        'content',
+        'build',
+        'ci',
+        'hotfix',
+        'bugfix',
+        'release'
+      ]
+    ],
+    // require a scope (optional—remove if you don’t want to enforce)
+    'scope-empty': [2, 'never'],
+  },
+  parserPreset: {
+    parserOpts: {
+      headerCorrespondence: [
+        'type',
+        'scope',
+        'subject'
+      ]
+    }
+  },
+  ignores: [
+    (message) => message.includes('Draft:')
+  ]
+
+};
